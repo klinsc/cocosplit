eef68b5e87a7069e427ebdc72dc1125fdf871a7f
klinsc <klinscea@gmail.com>
2023-12-05 11:21:18 +0700

    Update cocosplitV2.py

--- cocosplitV2.py
+++ cocosplitV2.py
@@ -1,4 +1,5 @@
 # read coco_annotations.json of Coco dataset and split it into train and test sets, using StratifiedShuffleSplit
+# also filter out classes that has only one sample, because it can't be split into the training and testing sets
 
 import argparse
 import json
@@ -19,8 +20,18 @@ def parse_args():
         type=str,
         help="Path to COCO annotations file.",
     )
-    parser.add_argument("train", type=str, help="Where to store COCO training annotations", default='train.json')
-    parser.add_argument("test", type=str, help="Where to store COCO test annotations", default='test.json')
+    parser.add_argument(
+        "train",
+        type=str,
+        help="Where to store COCO training annotations",
+        default="train.json",
+    )
+    parser.add_argument(
+        "val",
+        type=str,
+        help="Where to store COCO val annotations",
+        default="val.json",
+    )
     parser.add_argument(
         "--having-annotations",
         dest="having_annotations",
@@ -63,6 +74,31 @@ def filter_images(images, annotations):
     return funcy.lfilter(lambda a: int(a["id"]) in annotation_ids, images)
 
 
+def filter_one_sample_classes(annotations, categories):
+    """
+    Filter out classes that has only one sample, because it can't be split into the training and testing sets
+    """
+    category_ids = funcy.lmap(lambda c: int(c["id"]), categories)
+    annotations_per_category = {id: [] for id in category_ids}
+
+    for annotation in annotations:
+        annotations_per_category[int(annotation["category_id"])].append(annotation)
+
+    annotations_per_category = {
+        id: annotations
+        for id, annotations in annotations_per_category.items()
+        if len(annotations) > 1
+    }
+
+    category_ids = list(annotations_per_category.keys())
+    annotations = funcy.lfilter(
+        lambda a: int(a["category_id"]) in category_ids, annotations
+    )
+    categories = funcy.lfilter(lambda c: int(c["id"]) in category_ids, categories)
+
+    return annotations, categories
+
+
 def main():
     args = parse_args()
 
@@ -74,24 +110,43 @@ def main():
     categories = coco["categories"]
 
     if args.having_annotations:
-        images = filter_images(images, annotations)
         annotations = filter_annotations(annotations, images)
+        images = filter_images(images, annotations)
 
     image_ids = funcy.lmap(lambda i: int(i["id"]), images)
-    image_ids = np.array(image_ids)
+    category_ids = funcy.lmap(lambda c: int(c["id"]), categories)
 
-    # Stratified split
-    splitter = StratifiedShuffleSplit(n_splits=1, test_size=args.split)
-    train_idx, test_idx = next(splitter.split(image_ids, image_ids))
+    annotations_per_category = {id: [] for id in category_ids}
+    for annotation in annotations:
+        annotations_per_category[int(annotation["category_id"])].append(annotation)
 
-    train_images = funcy.lremove(lambda i: int(i["id"]) not in image_ids[train_idx], images)
-    test_images = funcy.lremove(lambda i: int(i["id"]) not in image_ids[test_idx], images)
+    train_annotations = []
+    test_annotations = []
 
-    train_annotations = filter_annotations(annotations, train_images)
-    test_annotations = filter_annotations(annotations, test_images)
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=args.split, random_state=0)
+    for category_id, annotations in annotations_per_category.items():
+        train_index, test_index = next(
+            sss.split(np.zeros(len(annotations)), np.zeros(len(annotations)))
+        )
+
+        for index in train_index:
+            train_annotations.append(annotations[index])
+
+        for index in test_index:
+            test_annotations.append(annotations[index])
+
+    train_image_ids = list(
+        set(funcy.lmap(lambda a: int(a["image_id"]), train_annotations))
+    )
+    test_image_ids = list(
+        set(funcy.lmap(lambda a: int(a["image_id"]), test_annotations))
+    )
+
+    train_images = funcy.lremove(lambda i: int(i["id"]) not in train_image_ids, images)
+    test_images = funcy.lremove(lambda i: int(i["id"]) not in test_image_ids, images)
 
     save_coco(args.train, train_images, train_annotations, categories)
-    save_coco(args.test, test_images, test_annotations, categories)
+    save_coco(args.val, test_images, test_annotations, categories)
 
 
 if __name__ == "__main__":
